6b1f140974466c49847a0a8c50ce0aaf1e1f2258
Gabriel Ostolaza <gabriel.ostolaza84@gmail.com>
2022-11-19 15:22:44 -0800

    added sandwich generator class

--- sandwich_api/sandwich_app/views.py
+++ sandwich_api/sandwich_app/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
+from django.http import Http404
+import random
+
+
 
 ingredients = {
     'Meats':['Turkey', 'Veggie Burger', 'Ham'],
@@ -17,6 +21,8 @@ class SandwichappView(View):
 
 class IngredientsListView(View):
     def get(self, request, ingredient_type):
+        if ingredient_type not in ingredients:
+            raise Http404(f'No such ingredient: {ingredient_type}')
         return render(
             request = request,
             template_name = 'ingredients_list.html',
@@ -25,3 +31,11 @@ class IngredientsListView(View):
                 'ingredient_type': ingredient_type,
             }
         )
+
+class SandwichGeneratorView(View):
+    def get(self, request):
+        selected_meat = random.choice(ingredients['meats'])
+        selected_cheese = random.choice(ingredients['cheeses'])
+        selected_toppings = random.choice(ingredients['toppings'])
+        sandwich = f'{selected_meat} & {selected_cheese} with {selected_toppings}'
+        return render(request, 'sandwich_generator.html', context = { 'sandwich' : sandwich})
