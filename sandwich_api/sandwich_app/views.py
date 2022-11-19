2950649de8fac08f7fe04a76b4412c2fc33075fb
Gabriel Ostolaza <gabriel.ostolaza84@gmail.com>
2022-11-19 15:03:38 -0800

    added ingredients list view

--- sandwich_api/sandwich_app/views.py
+++ sandwich_api/sandwich_app/views.py
@@ -14,3 +14,14 @@ class SandwichappView(View):
             template_name = 'sandwichapp.html',
             context = {'ingredients': ingredients.keys()}
         )
+
+class IngredientsListView(View):
+    def get(self, request, ingredient_type):
+        return render(
+            request = request,
+            template_name = 'ingredients_list.html',
+            context= {
+                'ingredients': ingredients[ingredient_type],
+                'ingredient_type': ingredient_type,
+            }
+        )
