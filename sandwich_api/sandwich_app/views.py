671c9f30213dc523c6866f84ca5bfed4d582dfa6
Gabriel Ostolaza <gabriel.ostolaza84@gmail.com>
2022-11-19 14:16:55 -0800

    added Sandwich app class

--- sandwich_api/sandwich_app/views.py
+++ sandwich_api/sandwich_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class Sandwich_app_View(View):
+    def get(self, request):
+        return render(
+            request=request,
+            template_name= 'sandwich_app.html'
+        )
