52ad4be0620a1650eb8706f41f13765a442139e9
Gabriel Ostolaza <gabriel.ostolaza84@gmail.com>
2022-11-19 15:39:37 -0800

    added changes

--- sandwich_api/sandwich_app/urls.py
+++ sandwich_api/sandwich_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     #sandwich/
     path('', SandwichappView.as_view(), name ='sandwich'),
 # sandwich/ingredients/<str:ingredient_type>
-    path('ingredients/<str:ingredient_type>', IngredientsListView.as_view(), name='ingredients_list'),],
+    path('ingredients/<str:ingredient_type>', IngredientsListView.as_view(), name='ingredients_list'),
     path('random', SandwichGeneratorView.as_view(), name='sandwich_generator')
+]
 
