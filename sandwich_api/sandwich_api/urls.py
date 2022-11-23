d1b7fae0c5884237b3279e5903b6bd5435d3af4f
Gabriel Ostolaza <gabriel.ostolaza84@gmail.com>
2022-11-22 17:26:43 -0800

    changes to file

--- sandwich_api/sandwich_api/urls.py
+++ sandwich_api/sandwich_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sandwich/', include('sandwich_app.urls'))
+    path('sandwich/', include('sandwich_app.urls')),
 ]
